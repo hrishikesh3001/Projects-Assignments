ee92daac04df91950616f7cf73da34214aec25ca
hrishikesh3001 <63711774+hrishikesh3001@users.noreply.github.com>
2020-06-20 13:33:52 +0530

    Created Deleting_dict.py
    It deletes a specific pair from the given dictionary.

---
+++ deleting_dict.py
@@ -0,0 +1,10 @@
+dict1 = {"Revanth" : 19, "Rohit" : 20, "Hrishi" : 19, "Ritish" : 20} 
+  
+# dictionary before removal 
+print ("Dictionary before : " + str(dict1)) 
+  
+del dict1['Hrishi'] 
+  
+# Printing dictionary after removal 
+print ("Dictionary after : " + str(dict1)) 
+  
